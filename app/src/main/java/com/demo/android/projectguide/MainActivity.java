8562a4ad0f5f0513daeafecd15fbd6fc7b739b4f
r2kluh <r2kluh@gmail.com>
2016-06-30 16:38:49 +0800

    add button and onClick listener

--- app/src/main/java/com/demo/android/projectguide/MainActivity.java
+++ app/src/main/java/com/demo/android/projectguide/MainActivity.java
@@ -1,7 +1,11 @@
 package com.demo.android.projectguide;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonMovie = (Button) findViewById(R.id.button_movies);
+        buttonMovie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = getApplicationContext();
+                CharSequence text = "this will launch Movies app";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
     }
+
+
+
+
 }
